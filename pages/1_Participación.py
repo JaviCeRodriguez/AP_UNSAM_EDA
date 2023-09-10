2636e316d265d7ce43a7264ab63b5d062658f8de
Javier Rodriguez <javicerodriguez@gmail.com>
2023-09-09 23:35:25 -0300

    add: new datasets about examns and graphs

--- pages/1_Participación.py
+++ pages/1_Participación.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.figure_factory as ff
 import plotly.express as px
 
 st.set_page_config(
@@ -9,26 +8,59 @@ st.set_page_config(
     layout="wide",
 )
 
-st.title("Participación")
+st.title("✋ Participación")
 
 df_inicial = pd.read_excel('./data/inscriptos/INICIAL.xlsx')
-st.dataframe(df_inicial)
 
+# Limpieza de valores erróneos
+provincias_no_validas = ['LA PLATA', 'EXALTACION DE LA CRUZ']
+df_inicial.PROVINCIA.replace(provincias_no_validas, 'BUENOS AIRES', inplace=True)
+
+st.dataframe(data=df_inicial, use_container_width=True)
 
 st.write("""
-## Geografía (por provincia)
+## 👶 Participación inicial
+
+Observemos los datos de participación al programa inicialmente. Se incluyen a todos los estudiantes que realizaron la
+inscripción, hayan rendido o no, o abandaron antes de arrancar.
 """)
+
+st.write("### Geografía (por provincia)")
 
 provincia_counts = df_inicial.PROVINCIA.value_counts()
 df_provincia = pd.DataFrame({
     "Provincia": provincia_counts.index,
     "Cantidad": provincia_counts.values,
 })
-fig = px.bar(
+fig_participacion = px.bar(
     data_frame=df_provincia,
     y="Cantidad",
     x="Provincia",
     color="Provincia",
-    title='Porcentaje de Estudiantes por Provincia'
+    title='Cantidad de estudiantes por Provincia'
 )
-st.plotly_chart(figure_or_data=fig, use_container_width=True)
+st.plotly_chart(figure_or_data=fig_participacion, use_container_width=True)
+
+
+st.write("### Nivel educativo")
+
+edu_counts = df_inicial.NIVELEDUCATIVO.value_counts()
+df_edu = pd.DataFrame({
+    "Nivel educativo": edu_counts.index,
+    "Cantidad": edu_counts.values
+})
+fig_edu = px.bar(
+    data_frame=df_edu,
+    y="Cantidad",
+    x="Nivel educativo",
+    color="Nivel educativo",
+    title='Cantidad de estudiantes por Nivel educativo'
+)
+st.plotly_chart(figure_or_data=fig_edu, use_container_width=True)
+
+
+st.write(""""
+## 👴 Participación final (3er módulo)
+
+Observemos los datos de participación en el último módulo hasta el día 08/09/2023.
+""")
